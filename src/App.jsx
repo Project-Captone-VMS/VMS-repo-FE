fab48c7aafa9ef46579944509bab1303547e29e2
Le Phuoc Hoang Lanh <hlanh.20082k3@gmail.com>
2024-10-19 23:40:39 +0700

    feat: update App file

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,24 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Login from "./pages/Authentication/Login";
+import Register from "./pages/Authentication/Register";
+
 function App() {
-  return <>Hoanglanh</>;
+  const router = createBrowserRouter([
+    {
+      path: "/Login",
+      element: <Login />,
+    },
+    {
+      path: "/Register",
+      element: <Register />,
+    },
+  ]);
+
+  return (
+    <>
+      <RouterProvider router={router} />
+    </>
+  );
 }
 
 export default App;
